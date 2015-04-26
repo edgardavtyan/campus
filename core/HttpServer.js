de7f247a90a7b961da62db8f88dfd14a93eec9ae
Edgar Davtyan <edgar.davtyan@outlook.com>
2015-04-26 11:15:59 +0300

    Add controller handling

--- core/HttpServer.js
+++ core/HttpServer.js
@@ -1,13 +1,33 @@
 var http = require('http');
+var Response = require('./Response');
+var Request = require('./Request');
 
 var HttpServer = function() {
    var self = this;
 
+   self.controllers = {};
+
    self.start = function() {
       http.createServer(function(req, res) {
-         res.writeHead(404, { 'Content-Type': 'text/plain' });
-         res.write('404 NOT FOUND');
-         res.end();
+         var response = new Response(res);
+         var request = new Request(req);
+
+         var controller = self.controllers[request.controllerName()];
+         if (!controller) {
+            response.send(404, 'text/plain', 'Controller not found');
+            return;
+         }
+
+         controller.response = response;
+         controller.request = request;
+
+         var controllerMethod = controller[request.controllerMethod()];
+         if (!controllerMethod) {
+            response.send(404, 'text/plain', 'Method not found');
+            return;
+         }
+
+         controllerMethod();
       }).listen(8888);
    };
 };
--- tests/HttpServer.js
+++ tests/HttpServer.js
@@ -2,14 +2,27 @@ var expect = require('expect.js');
 var http = require('http');
 var HttpServer = require('../core/HttpServer');
 
-describe('HttpServer', function() {
-   var requestData = {};
+var requestOptions = {};
 
+var makeRequest = function(onBodyRead) {
+   http.request(requestOptions, function(res) {
+      var body = '';
+      res.on('data', function(chunk) {
+         body += chunk;
+      });
+
+      res.on('end', function() {
+         onBodyRead(res, body);
+      });
+   }).end();
+};
+
+describe('HttpServer', function() {
    var httpServer = new HttpServer();
    httpServer.start();
 
    beforeEach(function() {
-      requestData = {
+      requestOptions = {
          host: 'localhost',
          port: '8888',
          path: '/',
@@ -17,10 +30,41 @@ describe('HttpServer', function() {
       };
    });
 
-   it('should respond with 404 by default', function(done) {
-      http.request(requestData, function(response) {
-         expect(response.statusCode).to.be(404);
+   it('should call index method of controller on request', function(done) {
+      var TestController = function() {
+         var self = this;
+
+         self.index = function() {
+            self.response.send(200, 'text/plain', 'Test Data');
+         };
+      };
+
+      httpServer.controllers['Test'] = new TestController();
+
+      requestOptions.path = '/Test/';
+      makeRequest(function(response, body) {
+         expect(response.statusCode).to.be(200);
+         expect(body).to.be('Test Data');
          done();
-      }).end();
+      });
+   });
+
+   it('should call non-index method of controller on request', function(done) {
+      var TestController = function() {
+         var self = this;
+
+         self.testMethod = function() {
+            self.response.send(200, 'text/plain', 'Test Data');
+         };
+      };
+
+      httpServer.controllers['Test'] = new TestController();
+
+      requestOptions.path = '/Test/testMethod/';
+      makeRequest(function(response, body) {
+         expect(response.statusCode).to.be(200);
+         expect(body).to.be('Test Data');
+         done();
+      });
    });
 });
