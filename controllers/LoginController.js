366ee62b1ade8feeb5fff3e54bb89611bd5dce66
Edgar Davtyan <edgar.davtyan@outlook.com>
2015-04-29 20:42:01 +0300

    Add very basic login to login page

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -4,6 +4,15 @@ var HomeController = function() {
    self.GET_index = function(req, res) {
       res.render('login');
    };
+
+   self.GET_auth = function(req, res) {
+      var apiUrl = 'http://campus-api.azurewebsites.net/User/Auth';
+      var apiData = { login: req.query().Username, password: req.query().Password };
+      console.log(apiData);
+      req.send('GET', apiUrl, apiData, function(apiResponse) {
+         res.send(200, 'text', apiResponse);
+      });
+   };
 };
 
 module.exports = HomeController;
