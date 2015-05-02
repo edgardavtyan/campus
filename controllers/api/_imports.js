4d3ca867d820ddd8545047c77a3f68ff5477babf
Edgar Davtyan <edgar.davtyan@outlook.com>
2015-05-02 22:25:09 +0300

    Add API server

---
+++ controllers/api/_imports.js
@@ -0,0 +1,5 @@
+var controllers = {
+
+};
+
+module.exports = controllers;
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var nunjucks = require('nunjucks');
 var HttpServer = require('./core/HttpServer');
 var frontendControllers = require('./controllers/frontend/_imports');
+var apiControllers = require('./controllers/api/_imports');
 
 
 nunjucks.configure('views', { autoescape: true });
@@ -8,3 +9,7 @@ nunjucks.configure('views', { autoescape: true });
 var frontendServer = new HttpServer();
 frontendServer.setControllers(frontendControllers);
 frontendServer.start(8888);
+
+var apiServer = new HttpServer();
+apiServer.setControllers(apiControllers);
+apiServer.start(1377);
