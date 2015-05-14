928c5ad56127eea7768337d2ad1cb932badde45f
Edgar Davtyan <edgar.davtyan@outlook.com>
2015-05-14 04:02:24 +0300

    Add UserController

---
+++ controllers/api/UserController.js
@@ -0,0 +1,16 @@
+var UserAccountModel = require('../../models/UserAccount.js');
+
+var HomeController = function() {
+   var self = this;
+
+
+   self.GET_auth = function(req, res) {
+      UserAccountModel
+         .find({ where: { Login: req.query().login } }, { raw: true })
+         .then(function(users) {
+            res.send(200, 'text', JSON.stringify(users, '', 4));
+         });
+   };
+};
+
+module.exports = new HomeController();
